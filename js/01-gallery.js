1d7d3b4fb68bb8c4c4fc1dd8eb3f83441ca6d5d0
Kamila Adamska <121kama121@gmail.com>
2023-04-01 22:15:18 +0200

    Made gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const galleryMarkup = [];
+
+function makeGallery(galleryObject) {
+  for (const galleryItem of galleryItems) {
+    const galleryItemMarkup = `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`;
+    galleryMarkup.push(galleryItemMarkup);
+  }
+  galleryListEl.innerHTML = galleryMarkup.join('');
+}
+
+makeGallery(galleryItems);
