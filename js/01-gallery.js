81d640dc8ba3976616b8584418cbc07527e009fd
Kamila Adamska <121kama121@gmail.com>
2023-04-02 01:26:59 +0200

    Added lightbox function to the gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,21 +4,35 @@ import { galleryItems } from './gallery-items.js';
 const galleryListEl = document.querySelector('.gallery');
 const galleryMarkup = [];
 
-function makeGallery(galleryObject) {
-  for (const galleryItem of galleryItems) {
+function makeGallery(galleryObjects) {
+  for (const galleryObject of galleryObjects) {
     const galleryItemMarkup = `<div class="gallery__item">
-  <a class="gallery__link" href="${galleryItem.original}">
+  <a class="gallery__link" href="${galleryObject.original}">
     <img
       class="gallery__image"
-      src="${galleryItem.preview}"
-      data-source="${galleryItem.original}"
-      alt="${galleryItem.description}"
+      src="${galleryObject.preview}"
+      data-source="${galleryObject.original}"
+      alt="${galleryObject.description}"
     />
   </a>
 </div>`;
+
     galleryMarkup.push(galleryItemMarkup);
   }
   galleryListEl.innerHTML = galleryMarkup.join('');
 }
+
+galleryListEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.target.src = event.target.dataset.source;
+
+  const lightbox = basicLightbox.create(
+    `<img src="${event.target.src}" alt="${event.target.alt}">`
+  );
+  event.target = lightbox.show();
+});
 
 makeGallery(galleryItems);
