20cfec34a6889ab344212fe729bd8c471a69bf76
Kamila Adamska <121kama121@gmail.com>
2023-04-02 17:20:12 +0200

    Made new gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+const galleryMarkup = [];
+
+function makeGallery(galleryObjects) {
+  for (const galleryObject of galleryObjects) {
+    const galleryItemMarkup = `<li class="gallery__item>
+<a class="gallery__item" href="${galleryObject.original}">
+  <img class="gallery__image" src="${galleryObject.preview}" alt="${galleryObject.description}" />
+</a>
+</li>`;
+
+    galleryMarkup.push(galleryItemMarkup);
+  }
+  galleryListEl.innerHTML = galleryMarkup.join('');
+}
+
+makeGallery(galleryItems);
